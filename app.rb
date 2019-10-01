ff94c037daa9f5ff473edd1cb3f0c26151cd8aca
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-10-01 17:16:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,19 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
-		
+		erb :index
 	end
+
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
+
 end
